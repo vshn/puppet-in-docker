c0758cb5d8ad6ba4bc584cefb51a9ab1d158571f
Tobias Brunner <tobias.brunner@vshn.ch>
2017-01-16 14:50:27 +0100

    support PuppetDB 2.3 connections for Puppetserver

--- puppetserver/docker-entrypoint.d/20-puppetdb.sh
+++ puppetserver/docker-entrypoint.d/20-puppetdb.sh
@@ -19,12 +19,21 @@ if [ -n "$PUPPETDB_SERVER_URL" ]; then
   done
   echo "---> PuppetDB ready at ${PUPPETDB_SERVER_URL}..."
 
-  echo "---> Configuring Puppetserver to connect to PuppetDB"
-  cat <<EOF>/etc/puppetlabs/puppet/puppetdb.conf
+  if [ "${USE_LEGACY_PUPPETDB}" == "true" ]; then
+    echo "---> Configuring Puppetserver to connect to PuppetDB (legacy config)"
+    cat <<EOF>/etc/puppetlabs/puppet/puppetdb.conf
+[main]
+server = ${PUPPETDB_SERVER_URL}
+port = 8081
+EOF
+  else
+    echo "---> Configuring Puppetserver to connect to PuppetDB"
+    cat <<EOF>/etc/puppetlabs/puppet/puppetdb.conf
 [main]
 server_urls = https://${PUPPETDB_SERVER_URL}
 soft_write_failure = true
 EOF
+  fi
   puppet config set storeconfigs_backend puppetdb --section main
   puppet config set storeconfigs true --section main
   puppet config set reports puppetdb --section main
