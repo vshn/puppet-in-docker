64164e70512163e336b609b525d8c4a656164046
Tobias Brunner <tobias.brunner@vshn.ch>
2017-01-18 11:35:44 +0100

    configure NATS during container startup

---
+++ nats/docker-entrypoint.d/20-nats-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CN=$(hostname)
+CERTFILE="/etc/nats/ssl/certs/${CN}.pem"
+KEYFILE="/etc/nats/ssl/private_keys/${CN}.pem"
+CAFILE="/etc/nats/ssl/certs/ca.pem"
+
+if [ -f ${CERTFILE} ]; then
+  echo "---> Configuring NATS certificates"
+  sed -i "s|CERTFILE|$CERTFILE|" /etc/nats/gnatsd.conf
+  sed -i "s|KEYFILE|$KEYFILE|" /etc/nats/gnatsd.conf
+  sed -i "s|CAFILE|$CAFILE|" /etc/nats/gnatsd.conf
+else
+  echo "---> Fatal: Certificate for NATS at ${CERTFILE} missing. Exiting."
+  exit 1
+fi
