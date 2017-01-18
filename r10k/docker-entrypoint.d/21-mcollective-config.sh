044e31b2745da83f32a2bd3af9ac123319cb9bd6
Tobias Brunner <tobias.brunner@vshn.ch>
2017-01-18 13:09:35 +0100

    configure mcollective on container startup

---
+++ r10k/docker-entrypoint.d/21-mcollective-config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export IDENTITY=$(hostname)
+export PUPPETCA=${PUPPETCA:-puppetca.local}
+export PUPPETDB=${PUPPETDB:-puppetdb.local}
+export PUPPETSERVER=${PUPPETSERVER:-puppetserver.local}
+export NATS=${NATS:-nats.local:4222}
+
+if [ -f /etc/puppetlabs/mcollective/plugin.d/choria.cfg ]; then
+  echo "---> Configuring MCollective Choria Plugin"
+  sed -i "s|PUPPETSERVER|$PUPPETSERVER|" /etc/puppetlabs/mcollective/plugin.d/choria.cfg
+  sed -i "s|PUPPETCA|$PUPPETCA|" /etc/puppetlabs/mcollective/plugin.d/choria.cfg
+  sed -i "s|PUPPETDB|$PUPPETDB|" /etc/puppetlabs/mcollective/plugin.d/choria.cfg
+  sed -i "s|NATS|$NATS|" /etc/puppetlabs/mcollective/plugin.d/choria.cfg
+else
+  echo "---> MCollective Choria plugin configuration file not found"
+fi
+
+if [ -f /etc/puppetlabs/mcollective/client.cfg ]; then
+  echo "---> Configuring MCollective Client"
+  sed -i "s|IDENTITY|$IDENTITY|" /etc/puppetlabs/mcollective/client.cfg
+fi
+
+if [ -f /etc/puppetlabs/mcollective/server.cfg ]; then
+  echo "---> Configuring MCollective Server"
+  sed -i "s|IDENTITY|$IDENTITY|" /etc/puppetlabs/mcollective/server.cfg
+fi
