5434b26393491bc81253493d8eadc0d23e1b1a85
marco.fretz <marco.fretz@vshn.ch>
2019-12-03 10:04:10 +0100

    Fix PUPPETSERVER_CATALOG_BACKEND default value

--- haproxy/docker-entrypoint.d/20-confd.sh
+++ haproxy/docker-entrypoint.d/20-confd.sh
@@ -5,7 +5,7 @@ export PUPPETCA_BACKEND=${PUPPETCA_BACKEND:-puppetca.local}
 export PUPPETDB_BACKEND=${PUPPETDB_BACKEND:-puppetdb.local}
 export PUPPETEXPLORER_BACKEND=${PUPPETEXPLORER_BACKEND:-puppetexplorer.local}
 export PUPPETSERVER_BACKEND=${PUPPETSERVER_BACKEND:-puppetserver.local}
-export PUPPETSERVER_CATALOG_BACKEND=${PUPPETSERVER_CATALOG_BACKEND:-puppetserver.local}
+export PUPPETSERVER_CATALOG_BACKEND=${PUPPETSERVER_BACKEND:-puppetserver.local}
 export SYSLOG_SERVER=${SYSLOG_SERVER:-localhost:2514}
 
 echo "---> Configuring HAProxy with confd"
